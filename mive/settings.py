79201299331ffc28bcf81c460f6f0a8d80686e0a
rakesh007007 <ry197089@gmail.com>
2015-08-12 09:29:47 -0400

    fg

--- mive/settings.py
+++ mive/settings.py
@@ -69,8 +69,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'veggex',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 # Internationalization
