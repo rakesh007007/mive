d60c3f83c68bef13fc6c8e2a662c2f54e6fe9df3
rakesh007007 <ry197089@gmail.com>
2015-08-20 08:18:45 -0400

    ids added

--- veggex/serializers.py
+++ veggex/serializers.py
@@ -6,18 +6,24 @@ from veggex.models import *
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Address
+        fields = ('address_id','area','city','state','pincode')
 class ProductsByCategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
+        fields = ('product_id', 'name', 'description',
+                  'category', 'popularityIndex', 'unit', 'priceType', 'pricePerUnit','origin','maxAvailableUnits','qualityRemarks','isPerishable','coverphotourl','origin')
 class ItemsOfCartSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cartitem
+        fields=('cartitem_id','cart','qtyInUnits','product')
 class OrdersOfUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
+        fields=('order_id','user','timeOfCreate','timeOfUpdate','payment_mode','subtotal','status')
 class ItemsOfOrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Orderitem
+        fields = ('orderitem_id','order','unit','qtyInUnits','priceType','priceAtThatTime','product')
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
@@ -31,6 +37,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class CartSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cart
+        fields = ('cart_id','timeOfCreate','timeOfUpdate')
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
@@ -39,12 +46,15 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
 class CartitemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cartitem
+        fields=('cartitem_id','cart','qtyInUnits','product')
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
+        fields=('order_id','user','timeOfCreate','timeOfUpdate','payment_mode','subtotal','status')
 class OrderitemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Orderitem
+        fields = ('orderitem_id','order','unit','qtyInUnits','priceType','priceAtThatTime','product')
 class SellerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Seller
