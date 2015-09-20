abf7d8f857a819bfdcd86ce17ba9996baa44ff56
Ubuntu google cloud <ubuntu@mive.in>
2015-09-20 12:59:51 +0530

    stash

---
+++ master.sh
@@ -0,0 +1,8 @@
+echo `sudo git add .`
+echo `sudo git commit -m "stash"`
+echo `sudo git stash`
+echo `sudo git merge origin master`
+echo `git add .`
+echo `git commit -m "merge"`
+echo `sudo git merge origin master`
+echo `sudo git stash`
