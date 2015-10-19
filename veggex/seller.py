44e0ac6fb1b079a30799f9e32ae43428503bc376
rakesh007007 <ry197089@gmail.com>
2015-10-19 18:40:13 +0530

    yoo

--- veggex/seller.py
+++ veggex/seller.py
@@ -4,54 +4,48 @@ def checklogin(request):
 		return False
 	else:
 		return True
+def get_or_create_csrf_token(request):
+	token = request.META.get('CSRF_COOKIE', None)
+	if token is None:
+		token = csrf._get_new_csrf_key()
+		request.META['CSRF_COOKIE'] = token
+	request.META['CSRF_COOKIE_USED'] = True
+	return token
 def basicinfo(request):
 	if(checklogin(request)==False):
 		return redirect('/login')
 	else:
-		miveuserId = request.session['miveuser']
-		miveuser = User.objects.get(user_id=int(miveuserId))
-		cart = miveuser.cart
-		cartItems = Cartitem.objects.filter(cart=cart)
-		totalItems = len(cartItems)
-		categoryvendor= miveuser.categories.filter(seller__status=1)
-		shippingCost=0
-		allProducts = []
-		for cvend in categoryvendor.all():
-			categoryvendor_id = cvend.categoryvendor_id
-			seller = cvend.seller
-			itemscount = Cartitem.objects.filter(product__seller = seller).filter(cart=cart).count()
-			if itemscount>0:
-				items = Cartitem.objects.filter(product__seller = seller).filter(cart=cart)
-				pd = {'categoryvendor_id':categoryvendor_id,'items':items,'seller':seller}
-				allProducts.append(pd)
-			else:
-				pass
-		print allProducts
-		final = {'miveuser':miveuser,'totalItems':totalItems,'categoryvendor':categoryvendor,'allProducts':allProducts}
-		return final
+		mivesellerId = request.session['mivesellerId']
+		miveseller = Seller.rak.get(seller_id=int(mivesellerId))
+		orders =  Order.objects.filter(seller=miveseller)
+		return {'orders':orders,'miveseller':miveseller}
 def sel(request):
 	return TemplateResponse(request,'adminr/seller/login.html')
 def main(request):
 	if(checklogin(request)==False):
-		return redirect('/login')
+		return redirect('seller/login')
 	else:
 		basics = basicinfo(request)
-		return TemplateResponse(request, 'adminr/index.html',{'basics':basics,'csrf_token':get_or_create_csrf_token(request)})
+		return TemplateResponse(request, 'adminr/seller/index.html',{'basics':basics,'csrf_token':get_or_create_csrf_token(request)})
 def logPost(request):
 	try:
 		mobile=request.POST['mobile']
 		password = request.POST['password']
 		u = Seller.rak.filter(mobileNo=mobile)
+		print mobile
+		print password
+		print u
+		print check_password(password,u[0].password)
 		#uu=Cart.objects.raw('select * from `veggex_cart` natural join `veggex_user` where mobileNo='+str(mobile))
 		if(check_password(password,u[0].password)):
 			request.session['sellerloggedin']=True
 			request.session['mobile']=mobile
-			request.session['miveuser']=u[0].user_id
+			request.session['mivesellerId']=u[0].seller_id
 			miveuser = u[0]
 			strraw={"mobile":mobile,"password":password}
 			#return HttpResponse(str(strraw))
 			return redirect('/seller/main?notify=yes&type=success&title=Logged In&description=You have been logged in')
 		else:
 			return redirect('/seller/login?notify=yes&type=error&title=LogIn&description=Login has been failed please try with proper credentials')
-	except:
+	except Exception,e:
 		return redirect('/seller/login?notify=yes&type=notice&title=LogIn&description=Looks like you are not registered please contact info@mive.in')
